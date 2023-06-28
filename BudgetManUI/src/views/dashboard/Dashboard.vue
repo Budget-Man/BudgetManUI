74b90df33ff89a42fd3c82df1d5040d26fb799a4
sy.nguyen <sy.nguyen@s3corp.com.vn>
2023-06-28 10:08:32 +0700

    add bootstrap

---
+++ BudgetManUI/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+      <h1>Welcome to the Project Budget Manager Dashboard</h1>
+  
+      <div class="summary">
+        <h2>Project Summary</h2>
+        <p>Total Budget: {{ totalBudget }}</p>
+        <p>Total Expenses: {{ totalExpenses }}</p>
+        <p>Remaining Budget: {{ remainingBudget }}</p>
+      </div>
+  
+      <div class="expenses">
+        <h2>Expense List</h2>
+        <ul>
+          <li v-for="expense in expenses" :key="expense.id">
+            {{ expense.description }} - ${{ expense.amount }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        totalBudget: 10000,
+        totalExpenses: 5000,
+        expenses: [
+          { id: 1, description: 'Office Supplies', amount: 500 },
+          { id: 2, description: 'Software License', amount: 2000 },
+          { id: 3, description: 'Marketing Campaign', amount: 2500 },
+        ],
+      };
+    },
+    computed: {
+      remainingBudget() {
+        return this.totalBudget - this.totalExpenses;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .summary {
+    margin-bottom: 20px;
+  }
+  
+  .expenses ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  </style>
---
+++ BudgetManUI/src/views/login/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+      <h1>Login</h1>
+      <form @submit="submitForm">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" id="username" v-model="username" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" v-model="password" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      submitForm(event) {
+        event.preventDefault();
+        // Perform login logic here
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+  </style>
