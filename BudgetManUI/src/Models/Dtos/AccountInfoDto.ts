66640663c2b91a1809ea20dd048af35d06a4e481
binh.phan@s3corp.com.vn <binh.phan@s3corp.com.vn>
2023-08-09 09:37:40 +0700

    add new Dto

---
+++ BudgetManUI/src/Models/Dtos/AccountInfoDto.ts
@@ -0,0 +1,8 @@
+export class AccountInfoDto {
+    Id: string | undefined;
+    UserId: string | undefined;
+    Name: string | undefined;
+    MobileNo: string | undefined;
+    Email: string | undefined;
+    Balance: number | undefined;
+}
