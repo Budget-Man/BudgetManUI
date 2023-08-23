6e7c16a95ef4dc2d17d0c99a3af5b3ac0f7d24aa
binh.phan@s3corp.com.vn <binh.phan@s3corp.com.vn>
2023-08-23 17:32:37 +0700

    change table to el-table

--- BudgetManUI/src/components/maynghien/adminTable/MnTable.vue
+++ BudgetManUI/src/components/maynghien/adminTable/MnTable.vue
@@ -1,36 +1,48 @@
 <template>
     <div>
-        <table>
-            <thead>
-                <tr>
-                    <th v-for="column in columns" :key="column.key">{{ column.label }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in datas" :key="item.id">
-                    <td v-for="column in columns" :key="column.key">{{ getValue(item, column.key) }}</td>
-                </tr>
-            </tbody>
-        </table>
+        <el-table class="admin-table" data="columns"  @sort-change="handleSortChange">
+            <el-table-column v-for="column in columns" :key="column.key" prop="{{column.key}}" label="{{column.label}}" 
+            sortable="{{ column.sorable }}" width="{{getWidth(column) }}" />
+           
+        </el-table>
     </div>
 </template>
   
 <script setup lang="ts">
+const props = defineProps<{
+    columns: TableColumn[];
+    datas: DTOItem[];
+}>();
 
 interface TableColumn {
     key: string;
     label: string;
+    width: number | undefined;
+    sortable:boolean|undefined;
 }
 
 interface DTOItem {
     [key: string]: any;
 }
-const props = defineProps<{
-  columns: TableColumn[];
-  datas: DTOItem[];
-}>();
 
+  // column: The column component
+  // prop: The property associated with the column
+  // order: 'ascending' or 'descending'
 
+  // Perform sorting logic here based on the prop and order
+  // You can update the tableData array with sorted data
+const handleSortChange = (column:any, prop:string, order:string ) => {
+
+};
+const getWidth = (column: TableColumn): string => {
+    let result: string = "";
+    if (column.width != undefined) {
+        result += column.width + "%";
+    }
+
+    return result;
+
+}
 const getValue = (item: DTOItem, key: string): string | number => {
     return item[key];
 };
@@ -38,5 +50,8 @@ const getValue = (item: DTOItem, key: string): string | number => {
   
 <style scoped>
 /* Add your table styling here */
+.admin-table {
+    width: 100%;
+}
 </style>
   
