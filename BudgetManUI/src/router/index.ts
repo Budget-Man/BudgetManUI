8e7b31615b4f959566c5ae15ded792ba4ebd07e1
Phan Thanh Binh <maynghien@hotmail.com>
2023-10-14 09:13:56 +0700

    fix bug login redirect

--- BudgetManUI/src/router/index.ts
+++ BudgetManUI/src/router/index.ts
@@ -4,7 +4,6 @@ import LayoutBlank from '../Layouts/LayoutBlank.vue';
 import LayoutElementVue from '../Layouts/LayoutElement.vue';
 import LayoutDN_DK from '../Layouts/LayoutDN_DK.vue'
 import LayoutMenuDoc from '../Layouts/LayoutMenuDoc.vue'
-import LOTest from '../Layouts/LOTest.vue'
 
 import Cookies from 'js-cookie';
 import HomeView from '../views/HomeView.vue'
@@ -51,8 +50,8 @@ const router = createRouter({
     },
   ],
 });
-// router.beforeEach((to, from, next) => {
-//   const isAuthenticated = !!Cookies.get('accessToken');
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!Cookies.get('accessToken');
   
   if (to.meta.requiresAuth && !isAuthenticated) {
     next('/login'); // Redirect to the login page
