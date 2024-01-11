0c09db871fa2f601e910a971ace0e2f7ada72353
Sỹ <synd276@gmail.com>
2024-01-11 13:28:32 +0700

    fix multiple language

--- BudgetManUI/src/languages/index.ts
+++ BudgetManUI/src/languages/index.ts
@@ -2,9 +2,10 @@ import { createI18n } from 'vue-i18n';
 // Import your language files
 import enLanguage from './en.json';
 import vnLanguage from './vn.json';
+import Cookies from 'js-cookie';
 
 export default createI18n({
-  locale: 'en',
+  locale: Cookies.get('language') || "en",
   fallbackLocale: 'en',
       messages: {
       en: enLanguage,
