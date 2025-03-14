8ac696826fa8c13a6eb5912a4c5a91ba25da07ad
maynghien <maynghien@gmail.com>
2025-03-14 17:02:25 +0700

    change deploy

--- BudgetManUI/src/Services/axiosConfig.ts
+++ BudgetManUI/src/Services/axiosConfig.ts
@@ -1,19 +1,15 @@
 import axios from "axios";
 import AxiosResponse from "axios";
-import router from '@/router'; // Import your Vue Router instance
-// import {useI18n} from 'vue-i18n';
-// import type { Composer } from 'vue-i18n';
+import router from '@/router';
 import languages from '@/languages'
 
-//  const baseAPIUrl = "https://localhost:7053/api/";
- const baseAPIUrl = "https://budgetmanbackendapi20231207220149.azurewebsites.net/api/";
- const axiosInstance = axios.create({
+const baseAPIUrl = import.meta.env.VITE_API_URL || "https://budgetmanbackendapi20231207220149.azurewebsites.net/api/";
+const axiosInstance = axios.create({
     baseURL: baseAPIUrl,
     timeout: 10000,
     headers: {'X-Custom-Header': 'foobar'}
-  });
+});
 
-  
 // Get the token from the cookies
 const getCookie = (name: string): string | null => {
   const value = `; ${document.cookie}`;
